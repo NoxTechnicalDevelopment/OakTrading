46a95819c5a6e9571bea1e96b55d62b6911d2a3c
Alex C <noxindustriescoding@gmail.com>
2025-10-08 15:05:59 -0700

    UI Tweaks

--- src/lib/BuyModal.svelte
+++ src/lib/BuyModal.svelte
@@ -148,11 +148,20 @@
 
     <form class="dialog__form" on:submit|preventDefault={submitBuy}>
       <label>
-        <span>Username</span>
+        <span>Roblox Display Name</span>
         <input
           bind:this={firstField}
           type="text"
           placeholder="Your in-game name"
+          autocomplete="username"
+        />
+      </label>
+      <label>
+        <span>Roblox Username</span>
+        <input
+          bind:this={firstField}
+          type="text"
+          placeholder="Your Roblox username"
           bind:value={username}
           autocomplete="username"
           required
